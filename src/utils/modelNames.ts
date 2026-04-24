25e248b16fa7033bbab45dccca301062420b43d3
Yash Sawarkar <yash.sawarkar@nimapinfotech.com>
2026-04-24 15:36:53 +0530

    add: modelNames file

--- src/utils/modelNames.ts
+++ src/utils/modelNames.ts
@@ -1,1 +1,10 @@
-// TODO: add an constant that contains the name of all the models in the project
+/**
+ * AUTO-GENERATED REGISTRY - DO NOT EDIT
+ * TO GENERATE OR UPDATE REGISTRY RUN 'npm run gen:models'
+ * Generated on: 2026-04-24T09:53:46.956Z
+ */
+export const modelNames = {
+  dot: 'Dot',
+} as const;
+
+export type ModelName = (typeof modelNames)[keyof typeof modelNames];
