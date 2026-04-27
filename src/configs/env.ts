360bd948cd08650df11562a093dff813a3490159
Yash Sawarkar <yash.sawarkar@nimapinfotech.com>
2026-04-27 14:20:11 +0530

    Update env.ts

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,4 +1,4 @@
-import { z as zod } from 'zod';
+import z, { z as zod } from 'zod';
 import * as dotenv from 'dotenv';
 import { environment } from '@/utils/enums/common.js';
 import { log_levels } from '@/utils/enums/pino.js';
@@ -6,9 +6,7 @@ import { log_levels } from '@/utils/enums/pino.js';
 dotenv.config();
 
 const envSchema = zod.object({
-  NODE_ENV: zod
-    .enum(Object.values(environment))
-    .default(environment.development),
+  NODE_ENV: zod.enum(Object.values(environment)).default(environment.development),
   PORT: zod.coerce.number().default(8080),
   LOG_LEVEL: zod.enum(Object.values(log_levels)).default(log_levels.info),
   MONGO_URI: zod.string(),
@@ -31,7 +29,7 @@ if (!_env.success) {
   // We use console.error here because the logger might not even be ready
   console.error(
     '❌ Invalid environment variables:',
-    JSON.stringify(_env.error.format(), null, 2),
+    JSON.stringify(z.treeifyError(_env.error), null, 2),
   );
   process.exit(1);
 }
